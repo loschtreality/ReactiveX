9bed3fa0bb66ee5c7e744b9013a40fce1dc81407
Michael Loschiavo <losch103@gmail.com>
2016-03-25 20:51:57 -0700

    Ex3 using forEach. Created new object on each loop containing ID and Title of each movie. Pushed into result array

---
+++ Exercise3.js
@@ -0,0 +1,56 @@
+function Ex3() {
+	var newReleases = [
+		{
+			"id": 70111470,
+			"title": "Die Hard",
+			"boxart": "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+			"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+			"rating": [4.0],
+			"bookmark": []
+		},
+		{
+			"id": 654356453,
+			"title": "Bad Boys",
+			"boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+			"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+			"rating": [5.0],
+			"bookmark": [{ id:432534, time:65876586 }]
+		},
+		{
+			"id": 65432445,
+			"title": "The Chamber",
+			"boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+			"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+			"rating": [4.0],
+			"bookmark": []
+		},
+		{
+			"id": 675465,
+			"title": "Fracture",
+			"boxart": "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+			"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+			"rating": [5.0],
+			"bookmark": [{ id:432534, time:65876586 }]
+		}
+	],
+	videoAndTitlePairs = [];
+
+	// ------------ INSERT CODE HERE! -----------------------------------
+	// Use forEach function to accumulate {id, title} pairs from each video.
+	// Put the results into the videoAndTitlePairs array using the Array's
+	// push() method. Example: videoAndTitlePairs.push(newItem);
+	// ------------ INSERT CODE HERE! -----------------------------------
+
+	newReleases.forEach(function(key){
+		var newItem = {};
+		newItem.id = key.id;
+		newItem.title = key.title;
+		videoAndTitlePairs.push(newItem);
+	});
+
+
+	return videoAndTitlePairs;
+}
+
+
+console.log(Ex3());
