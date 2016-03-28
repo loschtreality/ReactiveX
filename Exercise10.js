bca6f4c92bc5dd9b459b6f2d47ab06a37475bdf2
Michael Loschiavo <losch103@gmail.com>
2016-03-28 11:35:27 -0700

    built concatAll method

---
+++ Exercise10.js
@@ -0,0 +1,17 @@
+Array.prototype.concatAll = function() {
+	var results = [];
+	this.forEach(function(subArray) {
+		// ------------ INSERT CODE HERE! ----------------------------
+		// Add all the items in each subArray to the results array.
+		subArray.forEach(function(element){
+			results.push(element);
+		});
+		// ------------ INSERT CODE HERE! ----------------------------
+	});
+
+	return results;
+};
+
+var a = JSON.stringify([ [1,2,3], [4,5,6], [7,8,9] ].concatAll()); // === "[1,2,3,4,5,6,7,8,9]"
+// [1,2,3].concatAll(); // throws an error because this is a one-dimensional array
+console.log(a);
