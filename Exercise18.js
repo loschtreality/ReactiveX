b83d7419cd2319bb905f220ade3f405390414ec3
Michael Loschiavo <losch103@gmail.com>
2016-04-07 11:45:50 -0700

    Completed exercise 18. Used reduce to find object with largest dimentions, then map to target and return the url

--- Exercise18.js
+++ Exercise18.js
@@ -40,21 +40,16 @@ function Ex18() {
 	var boxarts = [
 			{ width: 200, height:200, url:"http://cdn-0.nflximg.com/images/2891/Fracture200.jpg" },
       { width: 425, height:150, url:"http://cdn-0.nflximg.com/images/2891/Fracture425.jpg" },
-			{ width: 150, height:200, url:"http://cdn-0.nflximg.com/images/2891/Fracture150.jpg" },
-			{ width: 300, height:200, url:"http://cdn-0.nflximg.com/images/2891/Fracture300.jpg" }
+      { width: 300, height:200, url:"http://cdn-0.nflximg.com/images/2891/Fracture300.jpg" },
+			{ width: 150, height:200, url:"http://cdn-0.nflximg.com/images/2891/Fracture150.jpg" }
 		];
 
 	// You should return an array containing only the URL of the largest box art. Remember that reduce always
 	// returns an array with one item.
 	return boxarts.reduce(function(acc,curr){
-    var sizeCurr = curr.width * curr.height;
-    var sizePrev = acc.width * acc.height;
-    if (sizePrev > sizeCurr) {
-      return acc.url;
-    }
-    else {
-      return curr.url;
-    }
+    return acc.width * acc.height > curr.width * curr.height ? acc : curr;
+  }).map(function(target) {
+  	return target.url;
   });  // Complete this expression
 }
 
