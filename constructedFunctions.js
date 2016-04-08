518f9d62605c3b4d57156909357895898016de0f
Michael Loschiavo <losch103@gmail.com>
2016-04-08 10:54:35 -0700

    Added file for constructed ReactiveX functions which change native behavior

---
+++ constructedFunctions.js
@@ -0,0 +1,78 @@
+//Exercise 4
+Array.prototype.map = function(projectionFunction) {
+	var results = [];
+	this.forEach(function(itemInArray) {
+    results.push(projectionFunction(itemInArray));
+	});
+
+	return results;
+};
+
+//Exercise 7
+Array.prototype.filter = function(predicateFunction) {
+	var results = [];
+	this.forEach(function(itemInArray) {
+    if (predicateFunction(itemInArray) === true) {
+      results.push(itemInArray);
+    }
+	});
+
+	return results;
+};
+
+//Exercise 10
+Array.prototype.concatAll = function() {
+	var results = [];
+	this.forEach(function(subArray) {
+		subArray.forEach(function(element){
+			results.push(element);
+		});
+	});
+	return results;
+};
+
+//Exercise 13
+Array.prototype.concatMap = function(projectionFunctionThatReturnsArray) {
+	return this.
+		map(function(item) {
+      return projectionFunctionThatReturnsArray(item);
+
+		}).
+		concatAll();
+};
+
+
+//Exercise 17
+Array.prototype.reduce = function(combiner, initialValue) {
+	var counter,
+		accumulatedValue;
+
+	// If the array is empty, do nothing
+	if (this.length === 0) {
+		return this;
+	}
+	else {
+		// If the user didn't pass an initial value, use the first item.
+		if (arguments.length === 1) {
+			counter = 1;
+			accumulatedValue = this[0];
+		}
+		else if (arguments.length >= 2) {
+			counter = 0;
+			accumulatedValue = initialValue;
+		}
+		else {
+			throw "Invalid arguments.";
+		}
+
+		// Loop through the array, feeding the current value and the result of
+		// the previous computation back into the combiner function until
+		// we've exhausted the entire array and are left with only one value.
+		while(counter < this.length) {
+			accumulatedValue = combiner(accumulatedValue, this[counter]);
+			counter++;
+		}
+
+		return [accumulatedValue];
+	}
+};
