5b2dc128aedb330ad484142e938914f783dbf262
Michael Loschiavo <losch103@gmail.com>
2016-04-20 10:52:33 -0700

    Attempt at implementing zip with previously defined methods. Getting Error zip is not a function

---
+++ Exercise24.js
@@ -0,0 +1,201 @@
+//Exercise 4
+Array.prototype.map = function(projectionFunction) {
+	var results = [];
+	this.forEach(function(itemInArray) {
+    results.push(projectionFunction(itemInArray));
+	});
+
+	return results;
+};
+
+//Exercise 7
+Array.prototype.filter = function(predicateFunction) {
+	var results = [];
+	this.forEach(function(itemInArray) {
+    if (predicateFunction(itemInArray) === true) {
+      results.push(itemInArray);
+    }
+	});
+
+	return results;
+};
+
+//Exercise 10
+Array.prototype.concatAll = function() {
+	var results = [];
+	this.forEach(function(subArray) {
+		subArray.forEach(function(element){
+			results.push(element);
+		});
+	});
+	return results;
+};
+
+//Exercise 13
+Array.prototype.concatMap = function(projectionFunctionThatReturnsArray) {
+	return this.
+		map(function(item) {
+      return projectionFunctionThatReturnsArray(item);
+
+		}).
+		concatAll();
+};
+
+
+//Exercise 17
+Array.prototype.reduce = function(combiner, initialValue) {
+	var counter,
+		accumulatedValue;
+
+	// If the array is empty, do nothing
+	if (this.length === 0) {
+		return this;
+	}
+	else {
+		// If the user didn't pass an initial value, use the first item.
+		if (arguments.length === 1) {
+			counter = 1;
+			accumulatedValue = this[0];
+		}
+		else if (arguments.length >= 2) {
+			counter = 0;
+			accumulatedValue = initialValue;
+		}
+		else {
+			throw "Invalid arguments.";
+		}
+
+		// Loop through the array, feeding the current value and the result of
+		// the previous computation back into the combiner function until
+		// we've exhausted the entire array and are left with only one value.
+		while(counter < this.length) {
+			accumulatedValue = combiner(accumulatedValue, this[counter]);
+			counter++;
+		}
+
+		return [accumulatedValue];
+	}
+};
+
+
+//Exercise 22
+Array.zip = function(left, right, combinerFunction) {
+	var counter,
+		results = [];
+
+	for(counter = 0; counter < Math.min(left.length, right.length); counter++) {
+		// Add code here to apply the combinerFunction to the left and right-hand items in the respective arrays
+		   results.push( combinerFunction(left[counter],right[counter]));
+	}
+
+	return results;
+};
+
+
+
+
+//This is a variation of the problem we solved earlier. This time each video has an interesting moments collection, each representing a time during which a screenshot is interesting or representative of the title as a whole. Notice that both the boxarts and interestingMoments arrays are located at the same depth in the tree. v
+
+
+
+function Ex24() {
+	var movieLists = [
+			{
+				name: "New Releases",
+				videos: [
+					{
+						"id": 70111470,
+						"title": "Die Hard",
+						"boxarts": [
+							{ width: 150, height:200, url:"http://cdn-0.nflximg.com/images/2891/DieHard150.jpg" },
+							{ width: 200, height:200, url:"http://cdn-0.nflximg.com/images/2891/DieHard200.jpg" }
+						],
+						"url": "http://api.netflix.com/catalog/titles/movies/70111470",
+						"rating": 4.0,
+						"interestingMoments": [
+							{ type: "End", time:213432 },
+							{ type: "Start", time: 64534 },
+							{ type: "Middle", time: 323133}
+						]
+					},
+					{
+						"id": 654356453,
+						"title": "Bad Boys",
+						"boxarts": [
+							{ width: 200, height:200, url:"http://cdn-0.nflximg.com/images/2891/BadBoys200.jpg" },
+							{ width: 140, height:200, url:"http://cdn-0.nflximg.com/images/2891/BadBoys140.jpg" }
+
+						],
+						"url": "http://api.netflix.com/catalog/titles/movies/70111470",
+						"rating": 5.0,
+						"interestingMoments": [
+							{ type: "End", time:54654754 },
+							{ type: "Start", time: 43524243 },
+							{ type: "Middle", time: 6575665}
+						]
+					}
+				]
+			},
+			{
+				name: "Instant Queue",
+				videos: [
+					{
+						"id": 65432445,
+						"title": "The Chamber",
+						"boxarts": [
+							{ width: 130, height:200, url:"http://cdn-0.nflximg.com/images/2891/TheChamber130.jpg" },
+							{ width: 200, height:200, url:"http://cdn-0.nflximg.com/images/2891/TheChamber200.jpg" }
+						],
+						"url": "http://api.netflix.com/catalog/titles/movies/70111470",
+						"rating": 4.0,
+						"interestingMoments": [
+							{ type: "End", time:132423 },
+							{ type: "Start", time: 54637425 },
+							{ type: "Middle", time: 3452343}
+						]
+					},
+					{
+						"id": 675465,
+						"title": "Fracture",
+						"boxarts": [
+							{ width: 200, height:200, url:"http://cdn-0.nflximg.com/images/2891/Fracture200.jpg" },
+							{ width: 120, height:200, url:"http://cdn-0.nflximg.com/images/2891/Fracture120.jpg" },
+							{ width: 300, height:200, url:"http://cdn-0.nflximg.com/images/2891/Fracture300.jpg" }
+						],
+						"url": "http://api.netflix.com/catalog/titles/movies/70111470",
+						"rating": 5.0,
+						"interestingMoments": [
+							{ type: "End", time:45632456 },
+							{ type: "Start", time: 234534 },
+							{ type: "Middle", time: 3453434}
+						]
+					}
+				]
+			}
+		];
+
+	//------------ COMPLETE THIS EXPRESSION --------------
+
+//Retrieve the time of the middle interesting moment and the smallest box art url simultaneously with zip(). Return an {id, title, time, url} object for each video.
+
+  return movieLists.
+		concatMap(function(movieList) {
+      return movieList.videos.zip(
+          //array 1
+        movieList.videos.filter(function(types){
+        return types.type === "Middle";
+      }),
+          //array 2
+        movieList.videos.reduce(function(prev,curr){
+          return (prev.width > curr.width) ? prev : curr;
+        }),
+        //callback
+        function (left,right) {
+          return left + right;
+        })
+
+		});
+
+}
+
+console.log(Ex24());
